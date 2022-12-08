1d023facd804b4e0aaef1913c4bcf3683245583e
Kamal-Aliyev <kamalliyev39@gmail.com>
2022-12-08 19:22:16 +0400

    Initial commit

---
+++ src/test/java/matrix/group/examtest/ExamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package matrix.group.examtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
